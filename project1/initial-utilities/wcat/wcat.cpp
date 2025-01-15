b34349e83d04191e39cd6533c6bf6ce4d9a7e601
angelajzhou <angelajmzhou@gmail.com>
2025-01-15 01:34:28 -0800

    crlf->lf AND wcat

--- project1/initial-utilities/wcat/wcat.cpp
+++ project1/initial-utilities/wcat/wcat.cpp
@@ -11,26 +11,34 @@ using namespace std;
 
 int main(int argc, char *argv[]){
     int fd;
-    if (argc == 1){
-        fd = STDIN_FILENO;
-    }
-    else{
-        fd = open(argv[1], O_RDONLY);
-        if (fd == -1){
-        cerr << "Error opening file: " << argv[1] << " (" << strerror(errno) << ")" << endl;
-
-            return 1;
-        }
-    }
     char buffer[4096];
     int ret;
     int bytesRead;
     stringstream stringStream;
-    while((bytesRead = read(fd, buffer, sizeof(buffer))) > 0){
-        ret = write(STDOUT_FILENO, buffer, bytesRead);
-        if (ret == -1){
-            cerr << "could not write to stdout." << endl;
+
+    if (argc == 1 && !isatty(STDIN_FILENO)){
+        fd = STDIN_FILENO; //this sets input to stdin if no file specified
+        while((bytesRead = read(fd, buffer, sizeof(buffer))) > 0){
+            ret = write(STDOUT_FILENO, buffer, bytesRead);
+            if (ret == -1){
+                cerr << "could not write to stdout." << endl;
+                return 1;
+            }
+        }
+    }
+
+    for (int i = 1; i<argc; i++){
+        fd = open(argv[i], O_RDONLY);
+        if (fd == -1){
+            cout << "wcat: cannot open file" << endl;
             return 1;
+        } 
+        while((bytesRead = read(fd, buffer, sizeof(buffer))) > 0){
+            ret = write(STDOUT_FILENO, buffer, bytesRead);
+            if (ret == -1){
+                cerr << "could not write to stdout." << endl;
+                return 1;
+            }
         }
     }
     return 0;
---
+++ project1/initial-utilities/wgrep/wgrep.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <sstream>
+#include <cstring>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    int fd;
+    char buffer[4096];
+    int ret;
+    int bytesRead;
+    stringstream stringStream;
+
+    if (argc == 1 && !isatty(STDIN_FILENO)){
+        fd = STDIN_FILENO; //this sets input to stdin if no file specified
+        while((bytesRead = read(fd, buffer, sizeof(buffer))) > 0){
+            ret = write(STDOUT_FILENO, buffer, bytesRead);
+            if (ret == -1){
+                cerr << "could not write to stdout." << endl;
+                return 1;
+            }
+        }
+    }
+
+    for (int i = 1; i<argc; i++){
+        fd = open(argv[i], O_RDONLY);
+        if (fd == -1){
+            cout << "wcat: cannot open file" << endl;
+            return 1;
+        } 
+        while((bytesRead = read(fd, buffer, sizeof(buffer))) > 0){
+            ret = write(STDOUT_FILENO, buffer, bytesRead);
+            if (ret == -1){
+                cerr << "could not write to stdout." << endl;
+                return 1;ya
+            }
+        }
+    }
+    return 0;
+}
