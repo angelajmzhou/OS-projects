04a98bc8d2124cc375e3daf8436d1e78533ef11d
angelajzhou <155860768+angelajmzhou@users.noreply.github.com>
2025-01-15 22:42:04 +0000

    wgrep progress

--- project1/initial-utilities/wgrep/wgrep.cpp
+++ project1/initial-utilities/wgrep/wgrep.cpp
@@ -9,52 +9,75 @@
 
 using namespace std;
 
+bool isitgreppy(string line, string grep);
 //note: GNU grep uses boyer-moore algo-- look into this!
 
 int main(int argc, char *argv[]){
     int fd;
     char buffer[4096];
-    int ret;
-    int newline;
+    string line = "";
+    long unsigned int nind;
     int bytesRead;
-    string line;
-
     //read stdin if no input file given (and is not terminal)
-    if (argc == 1 && !isatty(STDIN_FILENO)){
-        fd = STDIN_FILENO; //this sets input to stdin if no file specified
-    }
-
-    for (int i = 1; i<argc; i++){
-        /*
-        overall logic: read into buffer, append to string
-        find newline char in string, get rid of all after that
-        then move filepointer back
-
-        then, for each string to be grep-d, see if it's in that line
-        if it is, print it out :3
-        */
-        fd = open(argv[i], O_RDONLY);
-        if (fd == -1){
-            cout << "wgrep: cannot open file" << endl;
-            return 1;
-        } 
+    if (argc == 1){
+        cout<<"wgrep: searchterm [file ...]"<<endl;
+        return 1;
+    } else if (argc == 2 && !isatty(STDIN_FILENO)){
+        fd = STDIN_FILENO;
         while((bytesRead = read(fd, buffer, sizeof(buffer))) > 0){
-            line += buffer;
-            //newline in buffer
-            if((newline == line.find_first_of('\n')) != string::npos){
-                 = line.substr(0, newline);
-                line = line.substr(newline);
-            };
-            ret = write(STDOUT_FILENO, line.c_str(), line.size());
-            if (ret == -1){
-                cerr << "could not write to stdout." << endl;
+        line += buffer;
+        //nind in buffer
+        while((nind = line.find_first_of('\n')) != string::npos){
+            //this is running infinitely
+            if(!isitgreppy(line.substr(0, nind), argv[1])){
                 return 1;
             }
+            line = line.substr(nind);
+            cout<<line<<endl;
+            }
         }
+    } else{
+        for (int i = 2; i<argc; i++){
+            /*
+            overall logic: read into buffer, append to string
+            find newline char in string, get rid of all after that
+            then move filepointer back 
+
+            then, for each string to be grep-d, see if it's in that line
+            if it is, print it out :3
+            */
+            fd = open(argv[i], O_RDONLY);
+            if (fd == -1){
+                cout << "wgrep: cannot open file" << endl;
+                return 1;
+            }
+            while((bytesRead = read(fd, buffer, sizeof(buffer))) > 0){
+            line += buffer;
+            //nind in buffer
+            //runnign inf
+            while((nind = line.find_first_of('\n')) != string::npos){
+                if(!isitgreppy(line.substr(0, nind), argv[1])){
+                    return 1;
+                }
+                line = line.substr(nind);
+                cout<<line<<endl;
+
+                }
+            }
+        } 
     }
     return 0;
 }
 
-bool hasString(){
-    
+bool isitgreppy(string line, string grep){
+    int ret;
+    if(line.find(grep) != string::npos){
+        ret = write(STDOUT_FILENO, line.c_str(), line.size());
+        cout<<"greppy out: "<<line<<endl;
+        if (ret == -1){
+            cerr << "could not write to stdout." << endl;
+            return false;
+        }
+    }
+    return true;
 }
