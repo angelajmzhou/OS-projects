1e9eb8b17652540640ac35e92339f12a05a769db
angelajzhou <angelajmzhou@gmail.com>
2025-01-14 16:10:10 -0800

    broken wcat

---
+++ project1/initial-utilities/wcat/wcat.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    int fd;
+    if (argc == 1){
+        fd = STDIN_FILENO;
+    }
+    else{
+        fd = open(argv[1], O_RDONLY);
+        if (fd == -1){
+            cerr << "Error opening " <<argv[1] << endl;
+            return 1;
+        }
+    }
+    char buffer[4096];
+    int ret;
+    int bytesRead;
+    stringstream stringStream;
+    while((bytesRead = read(fd, buffer, sizeof(buffer))) > 0){
+        ret = write(STDOUT_FILENO, buffer, bytesRead);
+        if (ret == -1){
+            cerr << "could not write to stdout." << endl;
+            return 1;
+        }
+    }
+    return 0;
+}
