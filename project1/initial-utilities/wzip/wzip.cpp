94e501a9c13d8d9c1eaf5c0cf5a40637cbcabb5d
angelajzhou <angelajmzhou@gmail.com>
2025-01-16 17:26:48 -0800

    fini

--- project1/initial-utilities/wzip/wzip.cpp
+++ project1/initial-utilities/wzip/wzip.cpp
@@ -7,51 +7,89 @@
 #include <cstring>
 #define SZ_BUFFIN 4000
 #define SZ_BUFFOUT 2000
+
 using namespace std;
 
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        cout << "wzip: file1 [file2 ...]" << endl;
+        return 1;
+    }
 
-int main(int argc, char *argv[]){
-    int fd;
     char buffIn[SZ_BUFFIN];
     char buffOut[SZ_BUFFOUT];
-    uint32_t nchar;
+    uint32_t nchar = 0;
     int bytesIn = 0;
     int buffInd = 0;
-    char currch;
-    if (argc == 1){
-        cout<<"wunzip: file1 [file2 ...]"<<endl;
-        return 1;
-    } else{
-        
-        for (int i = 1; i<argc; i++){
-            /*
-            overall logic: read into buffIn, zip into buffOut -- need it to treat input file stream as cts. 
-            */
-            fd = open(argv[i], O_RDONLY);
-            if (fd == -1){
-                cout << "wunzip: cannot open file" << endl;
+    int j = 0;
+    char currch = 0, prevch = 0;
+    bool firstChar = true;
+
+    for (int file = 1; file < argc; file++) {
+        int fd = open(argv[file], O_RDONLY);
+        if (fd == -1) {
+            cerr << "wzip: cannot open file " << argv[file] << endl;
+            return 1;
+        }
+
+        while ((bytesIn = read(fd, buffIn, SZ_BUFFIN)) > 0) {
+            buffInd = 0;
+
+            while (buffInd < bytesIn) {
+                currch = buffIn[buffInd];
+                buffInd++;
+
+                if (firstChar) {
+                    prevch = currch;
+                    nchar = 1;
+                    firstChar = false;
+                } else if (currch == prevch) {
+                    nchar++;
+                } else {
+                    if (j + 5 > SZ_BUFFOUT) {
+                        if (write(STDOUT_FILENO, buffOut, j) == -1) {
+                            cerr << "wzip: cannot write to stdout" << endl;
+                            close(fd);
+                            return 1;
+                        }
+                        j = 0;
+                    }
+                    memcpy(&buffOut[j], &nchar, sizeof(uint32_t));
+                    buffOut[j + 4] = prevch;
+                    j += 5;
+
+                    prevch = currch;
+                    nchar = 1;
+                }
+            }
+        }
+
+        if (bytesIn == -1) {
+            cerr << "wzip: error reading file " << argv[file] << endl;
+            close(fd);
+            return 1;
+        }
+
+        close(fd);
+    }
+
+    if (!firstChar) {
+        if (j + 5 > SZ_BUFFOUT) {
+            if (write(STDOUT_FILENO, buffOut, j) == -1) {
+                cerr << "wunzip: cannot write to stdout" << endl;
                 return 1;
             }
-            while((bytesIn = read(fd, buffIn, SZ_BUFFIN)) > 0){
-                int j;
-                while (buffInd < bytesIn && j < SZ_BUFFOUT) {
-                    currch = buffIn[buffInd++];
-                    nchar = 1;
-                    while (buffInd < bytesIn && buffIn[buffInd] == currch) {
-                        nchar++;
-                        buffInd++;
-                    }
-
-                    memcpy(&buffOut[j], &nchar, sizeof(uint32_t));
-                    buffOut[j + 4] = currch;
-                    j += 5;
-                }
-                if (write(STDOUT_FILENO, buffOut, j) == -1) {
-                        cerr << "wunzip: cannot write to stdout" << endl;
-                        return 1;
-                }
-            } 
+            j = 0;
         }
+        memcpy(&buffOut[j], &nchar, sizeof(uint32_t));
+        buffOut[j + 4] = prevch;
+        j += 5;
     }
+
+    if (write(STDOUT_FILENO, buffOut, j) == -1) {
+        cerr << "wunzip: cannot write to stdout" << endl;
+        return 1;
+    }
+
     return 0;
 }
---
+++ project1/initial-utilities/wzip/wzip2.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <cstring>
+#define SZ_BUFFIN 4000
+#define SZ_BUFFOUT 2000
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        cout << "wzip: file1 [file2 ...]" << endl;
+        return 1;
+    }
+
+    char buffIn[SZ_BUFFIN];
+    char buffOut[SZ_BUFFOUT];
+    uint32_t nchar = 0;
+    int bytesIn = 0;
+    int buffInd = 0;
+    int j = 0;
+    char currch = 0, prevch = 0;
+    bool firstChar = true;
+
+    for (int file = 1; file < argc; file++) {
+        int fd = open(argv[file], O_RDONLY);
+        if (fd == -1) {
+            cerr << "wzip: cannot open file " << argv[file] << endl;
+            return 1;
+        }
+
+        while ((bytesIn = read(fd, buffIn, SZ_BUFFIN)) > 0) {
+            buffInd = 0;
+
+            while (buffInd < bytesIn) {
+                currch = buffIn[buffInd];
+                buffInd++;
+
+                if (firstChar) {
+                    prevch = currch;
+                    nchar = 1;
+                    firstChar = false;
+                } else if (currch == prevch) {
+                    nchar++;
+                } else {
+                    if (j + 5 > SZ_BUFFOUT) {
+                        if (write(STDOUT_FILENO, buffOut, j) == -1) {
+                            cerr << "wzip: cannot write to stdout" << endl;
+                            close(fd);
+                            return 1;
+                        }
+                        j = 0;
+                    }
+                    memcpy(&buffOut[j], &nchar, sizeof(uint32_t));
+                    buffOut[j + 4] = prevch;
+                    j += 5;
+
+                    prevch = currch;
+                    nchar = 1;
+                }
+            }
+        }
+
+        if (bytesIn == -1) {
+            cerr << "wzip: error reading file " << argv[file] << endl;
+            close(fd);
+            return 1;
+        }
+
+        close(fd);
+    }
+
+    if (!firstChar) {
+        if (j + 5 > SZ_BUFFOUT) {
+            if (write(STDOUT_FILENO, buffOut, j) == -1) {
+                cerr << "wunzip: cannot write to stdout" << endl;
+                return 1;
+            }
+            j = 0;
+        }
+        memcpy(&buffOut[j], &nchar, sizeof(uint32_t));
+        buffOut[j + 4] = prevch;
+        j += 5;
+    }
+
+    if (write(STDOUT_FILENO, buffOut, j) == -1) {
+        cerr << "wunzip: cannot write to stdout" << endl;
+        return 1;
+    }
+
+    return 0;
+}
