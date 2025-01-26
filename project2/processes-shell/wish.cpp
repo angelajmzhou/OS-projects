2b82a65304aa8a086ae1d423ea30ff00d800814a
angelajzhou <angelajmzhou@gmail.com>
2025-01-26 11:52:28 +0000

    need to debug

--- project2/processes-shell/wish.cpp
+++ project2/processes-shell/wish.cpp
@@ -9,7 +9,6 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
-#include <algorithm>
 #include <iterator>
 #include <unistd.h>
 #include <sys/wait.h>
@@ -19,7 +18,8 @@ using namespace std;
 
 void handleBuiltIn(string cmd);
 void parseInput(string line);
-void parseCommand(string line);
+void parseCommand(string redir[], bool redirect);
+void checkRedirect(string line);
 void defaultError();
 
 vector<const char *> paths = {"/bin"};
@@ -91,35 +91,53 @@ void parseInput(string line){
         commands.push_back(command);
     }
 
+
     if(commands.size() == 1){
         handleBuiltIn(command);
     }
 
     for (long unsigned int i = 0; i < commands.size(); i++) {
         if (fork() == 0) {
-            parseCommand(commands[i]);
+            checkRedirect(commands[i]);
             exit(0);                 
         }
     }
 
-    int status;
-
     for (long unsigned int idx = 0; idx < commands.size(); idx++) {
         //https://stackoverflow.com/questions/27306764/capturing-exit-status-code-of-child-process
-        wait(&status);
-        if (WEXITSTATUS(status)==5){
-            exit(0);
-        }
+        wait(NULL);
+
     }
 }
 
-void parseCommand(string line){
+void checkRedirect(string line){
+    stringstream ss(line);
+    string tokens[2];
+    string tmp;
+    int pos = 0;
+    while (getline(ss,tmp, '&')) {
+        if(pos>=2){
+            defaultError();
+            return;
+        }
+        tokens[pos] = tmp;
+        pos++;
+    }
+
+    if(pos == 1){
+        parseCommand(tokens, false);
+    }
+    else if (pos == 2){
+        parseCommand(tokens, true);
+    }
+
+}
+void parseCommand(string redir[], bool redirect){
     //taken from: https://stackoverflow.com/questions/236129/how-do-i-iterate-over-the-words-of-a-string
-        istringstream iss(line);
+        istringstream iss(redir[0]);
         vector<string> tokens{istream_iterator<string>{iss},
                       istream_iterator<string>{}}; 
-        if(tokens.size()==0){return;}
-        else if(tokens[0] == "exit" || tokens[0] == "cd" || tokens[0] == "path") {handleBuiltIn(line);}
+        if(tokens[0] == "exit" || tokens[0] == "cd" || tokens[0] == "path") {handleBuiltIn(redir[0]);}
         else{
             for(long unsigned int i = 0; i<paths.size(); i++){
                 char path[(strlen(paths[i]) + tokens[0].size() + 2)]; //+2 for backslash n null terminator
@@ -130,11 +148,22 @@ void parseCommand(string line){
                         argsv[j] = tokens[j].c_str(); 
                     }
                     argsv[tokens.size()] = NULL;
+
                     if(fork() == 0){ //if child
                         //pointer to an constant pointer to a character
+                        if(redirect){
+                            int fd = open(redir[1].c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                            if (fd < 0) {
+                                defaultError();
+                                exit(0);
+                            }
+                            dup2(fd, STDOUT_FILENO);
+                            dup2(fd, STDERR_FILENO);
+                        }   
                         if(execv(path, const_cast<char* const*>(argsv)) == -1){ 
                             defaultError();
                         } 
+
                         exit(0);
                     } else{ //if parent, wait for child
                         if(wait(NULL) == -1){
