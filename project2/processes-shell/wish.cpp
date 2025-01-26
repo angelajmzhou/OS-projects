bf6c8a9492b7bcb11ce9ec7c4a4d88c707e2612e
angelajzhou <angelajmzhou@gmail.com>
2025-01-26 06:58:22 +0000

    finish parallelism

--- project2/processes-shell/wish.cpp
+++ project2/processes-shell/wish.cpp
@@ -17,7 +17,9 @@
 
 using namespace std;
 
-int parseInput(string line);
+void handleBuiltIn(string cmd);
+void parseInput(string line);
+void parseCommand(string line);
 void defaultError();
 
 vector<const char *> paths = {"/bin"};
@@ -38,83 +40,91 @@ int main(int argc, char *argv[]){
 
         }
         while (getline(file, line)) {
-             if(parseInput(line) == 1){break;}
+            parseInput(line);
         }
         exit(0);
     }
-
-    cout<<"wish> ";
-    while(getline(cin, line)){
-        cout<<"wish> ";
-
-        stringstream ss(line);
-        vector<string> commands;
-        string command;
-        //https://www.geeksforgeeks.org/how-to-split-string-by-delimiter-in-cpp/
-        while(getline(ss, command, '&')){
-            commands.push_back(command);
-        }
-        
-        for (long unsigned int i = 0; i<commands.size(); i++){
-            if(fork()==0){
-                parseInput(commands[i]);
-                exit(0);
-            } else{}
-        }
-        for (long unsigned int idx = 0; idx<commands.size(); idx++){
-		  wait(NULL); 
-		  /*
-		  calling for each child we create
-		  more principled way: keep vector of process and child ID's, and wait
-		  for them to exit. but this had the exact effect.
-		  cannot assume parent always runs first!
-		  */
-	  }
-        //priority: split by ampersands, then split by redirect, then parse the commands...
-        if(parseInput(line) == 1){
-            defaultError();
-        }
+    cout<<"wish> "<<flush;
+    while (getline(cin, line)) {
+        parseInput(line);
+        cout << "wish> " << flush;
     }
     exit(0);
+
 }
 
-int parseInput(string line){
+void handleBuiltIn(string cmd){
+    //handle our built-in commands
+    istringstream iss(cmd);
+    vector<string> tokens{istream_iterator<string>{iss}, istream_iterator<string>{}}; 
+    if (cmd == "exit"){
+        if(tokens.size()==1){
+            exit(0);
+        }
+        defaultError(); 
+    }
+    else if(cmd == "cd"){
+        if(tokens.size()==2){
+            if(chdir(tokens[1].c_str())==-1){
+                defaultError();
+            }
+        } else{
+            defaultError(); 
+        }
+    }
+    else if(cmd == "path"){
+        //set search path(s) here
+        paths.clear();
+        for(long unsigned int i = 1; i<tokens.size(); i++){
+            paths.push_back(tokens[i].c_str());
+        }
+        
+    }
+}
+
+void parseInput(string line){
+    stringstream ss(line);
+    vector<string> commands;
+    string command;
+
+    while (getline(ss, command, '&')) {
+        commands.push_back(command);
+    }
+
+    if(commands.size() == 1){
+        handleBuiltIn(command);
+    }
+
+    for (long unsigned int i = 0; i < commands.size(); i++) {
+        if (fork() == 0) {
+            parseCommand(commands[i]);
+            exit(0);                 
+        }
+    }
+
+    int status;
+
+    for (long unsigned int idx = 0; idx < commands.size(); idx++) {
+        //https://stackoverflow.com/questions/27306764/capturing-exit-status-code-of-child-process
+        wait(&status);
+        if (WEXITSTATUS(status)==5){
+            exit(0);
+        }
+    }
+}
+
+void parseCommand(string line){
     //taken from: https://stackoverflow.com/questions/236129/how-do-i-iterate-over-the-words-of-a-string
         istringstream iss(line);
         vector<string> tokens{istream_iterator<string>{iss},
                       istream_iterator<string>{}}; 
-        if(tokens.size()==0){return 0;}
-        string cmd;
-        cmd = tokens[0];
-        //handle our built-in commands
-        if (cmd == "exit"){
-            if(tokens.size()==1){
-                exit(0);
-            }
-            defaultError(); 
-        }
-        else if(cmd == "cd"){
-            if(tokens.size()==2){
-                if(chdir(tokens[1].c_str())==-1){
-                    defaultError();
-                }
-            } else{
-                defaultError(); 
-            }
-        }
-        else if(cmd == "path"){
-            //set search path(s) here
-            paths.clear();
-            for(long unsigned int i = 1; i<tokens.size(); i++){
-                paths.push_back(tokens[i].c_str());
-            }
-            
-        }
+        if(tokens.size()==0){return;}
+        else if(tokens[0] == "exit" || tokens[0] == "cd" || tokens[0] == "path") {handleBuiltIn(line);}
         else{
             for(long unsigned int i = 0; i<paths.size(); i++){
                 char path[(strlen(paths[i]) + tokens[0].size() + 2)]; //+2 for backslash n null terminator
                 snprintf(path, sizeof(path), "%s/%s", paths[i], tokens[0].c_str());
-                if(access(path, X_OK) == 0){
+                if(access(path, X_OK) == 0){ 
                     const char* argsv[tokens.size() + 1]; //pointer to an array of constant chars
                     for (size_t j = 0; j < tokens.size(); j++) {
                         argsv[j] = tokens[j].c_str(); 
@@ -123,19 +133,19 @@ int parseInput(string line){
                     if(fork() == 0){ //if child
                         //pointer to an constant pointer to a character
                         if(execv(path, const_cast<char* const*>(argsv)) == -1){ 
-                            return 1;
-                        } // Call execv
+                            defaultError();
+                        } 
+                        exit(0);
                     } else{ //if parent, wait for child
                         if(wait(NULL) == -1){
-                            return 1;
+                            defaultError();
                         }
                     }
-                    return 0;
+                    return;
                 }
             }
             defaultError();
         }
-        return 0;
 }
 
 void defaultError(){
